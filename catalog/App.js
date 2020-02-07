d7214baeb2df28d0ea246bc65204e4b100ead6c6
David Xu <david.xu@spark.co.nz>
2020-02-07 15:56:59 +1300

    upgrading to the latest version of set tokens

--- catalog/App.js
+++ catalog/App.js
@@ -1,35 +1,36 @@
-import React, {useState} from 'react'
-import pinkGreen from '@sparknz/set-tokens/dist/docs/pinkGreen.json';
-import purpleOrange from '@sparknz/set-tokens/dist/docs/purpleOrange.json';
-import halloween from '@sparknz/set-tokens/dist/docs/halloween.json';
+import React, { useState } from "react";
+import theDefault from "@sparknz/set-tokens/dist/docs/default.json";
+import halloween from "@sparknz/set-tokens/dist/docs/halloween.json";
 
-import pinkGreen_js from '@sparknz/set-tokens/dist/js/pinkGreen.json';
-import purpleOrange_js from '@sparknz/set-tokens/dist/js/purpleOrange.json';
-import halloween_js from '@sparknz/set-tokens/dist/js/halloween.json';
+import default_js from "@sparknz/set-tokens/dist/js/default.json";
+import halloween_js from "@sparknz/set-tokens/dist/js/halloween.json";
 
+import ThemeSelector from "./components/ThemeSelector";
+import CatalogRoot from "./components/CatalogRoot";
 
-
-import ThemeSelector from './components/ThemeSelector';
-import CatalogRoot from './components/CatalogRoot';
-
-export default function App(){
-    const [selectedTheme, onThemeChange] = useState('Pink Green');
+export default function App() {
+    const [selectedTheme, onThemeChange] = useState("Default");
     const themes = {
-        "Pink Green": pinkGreen,
-        "Purple Orange": purpleOrange,
-        "Halloween": halloween
+        Default: theDefault,
+        Halloween: halloween
     };
 
     const jsThemes = {
-        "Pink Green": pinkGreen_js,
-        "Purple Orange": purpleOrange_js,
-        "Halloween": halloween_js
-    }
+        Default: default_js,
+        Halloween: halloween_js
+    };
 
     return (
         <div>
-            <ThemeSelector onChange={e=>onThemeChange(e.target.value)} value={selectedTheme} options={Object.keys(themes)}/>
-            <CatalogRoot currentTheme={themes[selectedTheme]} jsTheme={jsThemes[selectedTheme]}/>
+            <ThemeSelector
+                onChange={e => onThemeChange(e.target.value)}
+                value={selectedTheme}
+                options={Object.keys(themes)}
+            />
+            <CatalogRoot
+                currentTheme={themes[selectedTheme]}
+                jsTheme={jsThemes[selectedTheme]}
+            />
         </div>
-    )
+    );
 }
--- catalog/components/CatalogRoot.js
+++ catalog/components/CatalogRoot.js
@@ -1,123 +1,163 @@
-import React from 'react';
+import React from "react";
 import { Catalog, pageLoader } from "catalog";
 
-import { getPropertyArray, getThemeColorArrays } from '../helpers/color';
+import { getPropertyArray } from "../helpers/color";
 
-const loader = (page) =>
-  pageLoader(() => import(`../docs/${page}.md`));
-
+const loader = page => pageLoader(() => import(`../docs/${page}.md`));
 
 export default function CatalogRoot({ currentTheme, jsTheme }) {
-
-  const pages = [
-    {
-      path: "/",
-      title: "Welcome",
-      content: loader("welcome"),
-      imports: { currentTheme: jsTheme }
-    },
-    {
-      title: "Color",
-      pages: [
+    const pages = [
         {
-          path: "/color/base",
-          title: "Base Color",
-          imports: { colors: getPropertyArray(currentTheme, 'color base') },
-          content: loader("color/base"),
+            path: "/",
+            title: "Welcome",
+            content: loader("welcome"),
+            imports: { currentTheme: jsTheme }
         },
         {
-          path: "/color/text",
-          title: "Text Color",
-          imports: { colorArray: getPropertyArray(currentTheme, 'color text') },
-          content: loader("color/text"),
+            title: "Color",
+            pages: [
+                {
+                    path: "/color/base",
+                    title: "Base Color",
+                    imports: {
+                        colors: getPropertyArray(currentTheme, "color base")
+                    },
+                    content: loader("color/base")
+                },
+                {
+                    path: "/color/text",
+                    title: "Text Color",
+                    imports: {
+                        colorArray: getPropertyArray(currentTheme, "color text")
+                    },
+                    content: loader("color/text")
+                },
+                {
+                    path: "/color/border",
+                    title: "Border Color",
+                    imports: {
+                        colorArray: getPropertyArray(
+                            currentTheme,
+                            "color border"
+                        )
+                    },
+                    content: loader("color/border")
+                },
+                {
+                    path: "/color/background",
+                    title: "Background Color",
+                    imports: {
+                        colorArray: getPropertyArray(
+                            currentTheme,
+                            "color background"
+                        )
+                    },
+                    content: loader("color/background")
+                },
+                {
+                    path: "/color/shadow",
+                    title: "Shadow Color",
+                    imports: {
+                        colorArray: getPropertyArray(
+                            currentTheme,
+                            "color shadow"
+                        )
+                    },
+                    content: loader("color/shadow")
+                }
+            ]
         },
         {
-          path: "/color/border",
-          title: "Border Color",
-          imports: { colorArray: getPropertyArray(currentTheme, 'color border') },
-          content: loader("color/border"),
+            title: "Layout",
+            pages: [
+                {
+                    path: "/layout/breakpoint",
+                    title: "Breakpoint",
+                    imports: {
+                        breakpoint: getPropertyArray(currentTheme, "breakpoint")
+                    },
+                    content: loader("layout/breakpoint")
+                },
+                {
+                    path: "/layout/z-index",
+                    title: "z-index",
+                    imports: {
+                        zIndex: getPropertyArray(currentTheme, "zIndex")
+                    },
+                    content: loader("layout/z-index")
+                }
+            ]
         },
         {
-          path: "/color/background",
-          title: "Background Color",
-          imports: { colorArray: getPropertyArray(currentTheme, 'color background') },
-          content: loader("color/background"),
+            title: "Sizing",
+            pages: [
+                {
+                    path: "/sizing/font",
+                    title: "Font",
+                    imports: {
+                        font: getPropertyArray(currentTheme, "font", "label")
+                    },
+                    content: loader("sizing/font")
+                },
+                {
+                    path: "/sizing/line_height",
+                    title: "Line Height",
+                    imports: {
+                        line_height: getPropertyArray(
+                            currentTheme,
+                            "line_height",
+                            "label"
+                        )
+                    },
+                    content: loader("sizing/line_height")
+                },
+                {
+                    path: "/sizing/spacing",
+                    title: "Spacing",
+                    imports: {
+                        spacing: getPropertyArray(currentTheme, "spacing")
+                    },
+                    content: loader("sizing/spacing")
+                },
+                {
+                    path: "/sizing/radius",
+                    title: "Border Radius",
+                    imports: {
+                        radiusArray: getPropertyArray(currentTheme, "radius")
+                    },
+                    content: loader("sizing/radius")
+                }
+            ]
         },
         {
-          path: "/color/shadow",
-          title: "Shadow Color",
-          imports: { colorArray: getPropertyArray(currentTheme, 'color shadow') },
-          content: loader("color/shadow"),
+            title: "Misc",
+            pages: [
+                {
+                    path: "/misc/opcacity",
+                    title: "Opacity",
+                    imports: {
+                        opacityArray: getPropertyArray(currentTheme, "opacity")
+                    },
+                    content: loader("misc/opacity")
+                },
+                {
+                    path: "/misc/duration",
+                    title: "Duration",
+                    imports: {
+                        duration: getPropertyArray(currentTheme, "duration")
+                    },
+                    content: loader("misc/duration")
+                }
+            ]
         }
-      ]
-    },
-    {
-      title: "Layout",
-      pages: [
-        {
-          path: "/layout/breakpoint",
-          title: "Breakpoint",
-          imports: { breakpoint: getPropertyArray(currentTheme, 'breakpoint') },
-          content: loader("layout/breakpoint"),
-        },
-        {
-          path: "/layout/z-index",
-          title: "z-index",
-          imports: { zIndex: getPropertyArray(currentTheme, 'zIndex') },
-          content: loader("layout/z-index"),
-        }
-      ]
-    },
-    {
-      title: "Sizing",
-      pages: [
-        {
-          path: "/sizing/font",
-          title: "Font",
-          imports: { font: getPropertyArray(currentTheme, 'font', 'label') },
-          content: loader("sizing/font"),
-        },
-        {
-          path: "/sizing/line_height",
-          title: "Line Height",
-          imports: { line_height: getPropertyArray(currentTheme, 'line_height', 'label') },
-          content: loader("sizing/line_height"),
-        },
-        {
-          path: "/sizing/spacing",
-          title: "Spacing",
-          imports: { spacing: getPropertyArray(currentTheme, 'spacing') },
-          content: loader("sizing/spacing"),
-        },
-        {
-          path: "/sizing/radius",
-          title: "Border Radius",
-          imports: {  radiusArray: getPropertyArray(currentTheme, 'radius')},
-          content: loader("sizing/radius"),
-        },
-      ]
-    },
-    {
-      title: "Misc",
-      pages: [
-        {
-          path: "/misc/opcacity",
-          title: "Opacity",
-          imports: { opacityArray: getPropertyArray(currentTheme, 'opacity') },
-          content: loader("misc/opacity"),
-        },
-        {
-          path: "/misc/duration",
-          title: "Duration",
-          imports: { duration: getPropertyArray(currentTheme, 'duration') },
-          content: loader("misc/duration"),
-        },
-      ]
-    }
-  ];
+    ];
 
-  return <Catalog title="Catalog" pages={pages} basePath='/SET-Docs'  useBrowserHistory={true}  />
-
+    return (
+        <Catalog
+            title="Catalog"
+            pages={pages}
+            basePath="/SET-Docs"
+            useBrowserHistory={true}
+        />
+    );
 }
-
-
